439f598e86b05787547762f68e5a25c98259bec1
Emmanuel Onyeozu <104470064+eminovaden@users.noreply.github.com>
2023-11-15 15:27:16 +0100

    Update estr_2.c

--- estr_2.c
+++ estr_2.c
@@ -1,140 +1,75 @@
 #include "emshell.h"
 
 /**
- * _strcpy - It copies a string
- * @dest: the destination
- * @src: the source
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
  *
- * Return: pointer to destination
+ * Return: integer length of string
  */
-char *_strcpy(char *dest, char *src)
+int _strlen(char *s)
 {
 	int i = 0;
 
-	if (dest == src || src == 0)
-		return (dest);
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = 0;
-	return (dest);
-}
-
-/**
- * *_strdup - copies the string given as parameter
- * @str: string to duplicate
- * Return: pointer to the copied string (Success), NULL (Error)
- */
-
-char *_strdup(const char *str)
-{
-	unsigned int i = 0, len = 0;
-	char *d;
-
-	if (str == NULL)
-		return (NULL);
-
-	while (str[len])
-		len++;
-
-	d = malloc(sizeof(char) * (len + 1));
-
-	if (d == NULL)
-		return (NULL);
-
-	while ((d[i] = str[i]) != 0)
-		i++;
-
-	return (d);
-}
-
-/**
- *_puts - prints an input string
- *@fstr: the string to be printed
- *
- * Return: Nothing
- */
-void _puts(char *fstr)
-{
-	int i = 0;
-
-	if (!fstr)
-		return;
-	while (fstr[i] != '\0')
-	{
-		_putchar(fstr[i]);
-		i++;
-	}
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	static int i;
-	static char buf[FBUF_SIZE];
-
-	if (c == FBUF_FLUSH || i >= FBUF_SIZE)
-	{
-		write(1, buf, i);
-		i = 0;
-	}
-	if (c != FBUF_FLUSH)
-		buf[i++] = c;
-	return (1);
-}
-
-/**
- * **strtow - splits a string into words. Repeat delimiters are ignored
- * @fstr: the input string
- * @d: the delimeter string
- * Return: a pointer to an array of strings, or NULL on failure
- */
-
-char **strtow(char *fstr, char *d)
-{
-	int i, j, k, m, numwords = 0;
-	char **s;
-
-	if (fstr == NULL || fstr[0] == 0)
-		return (NULL);
-	if (!d)
-		d = " ";
-	for (i = 0; fstr[i] != '\0'; i++)
-		if (!fdelim(fstr[i], d) && (fdelim(fstr[i + 1], d) || !fstr[i + 1]))
-			numwords++;
-
-	if (numwords == 0)
-		return (NULL);
-	s = malloc((1 + numwords) * sizeof(char *));
 	if (!s)
-		return (NULL);
-	for (i = 0, j = 0; j < numwords; j++)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp - performs lexicogarphic comparison of two strangs.
+ * @s1: the first strang
+ * @s2: the second strang
+ *
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
 	{
-		while (fdelim(fstr[i], d))
-			i++;
-		k = 0;
-		while (!fdelim(fstr[i + k], d) && fstr[i + k])
-			k++;
-		s[j] = malloc((k + 1) * sizeof(char));
-		if (!s[j])
-		{
-			for (k = 0; k < j; k++)
-				free(s[k]);
-			free(s);
-			return (NULL);
-		}
-		for (m = 0; m < k; m++)
-			s[j][m] = fstr[i++];
-		s[j][m] = 0;
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
 	}
-	s[j] = NULL;
-	return (s);
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * fstarts_with - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
+ *
+ * Return: address of next char of haystack or NULL
+ */
+char *fstarts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *fret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (fret);
 }
